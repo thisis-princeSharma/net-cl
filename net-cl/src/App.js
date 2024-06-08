f05a176d4e8abb1ef3f63dbd03b065f408ee09ce
PrinceSharma <121371635+thisis-princeSharma@users.noreply.github.com>
2024-06-08 23:45:54 +0000

    Done a lot of things

--- net-cl/src/App.js
+++ net-cl/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <Row title="Netflix ORIGINALS"/>
+      <Row title="Trending Now"/>
     </div>
   );
 }
---
+++ net-cl/src/requests.js
@@ -0,0 +1,6 @@
+const API_KEY = "28f35636469d2882105917ce3b53130f";
+
+const requests = {
+    
+}
+export default requests;

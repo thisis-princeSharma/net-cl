6ad9315a00d3acddef09da563361391528567673
PrinceSharma <121371635+thisis-princeSharma@users.noreply.github.com>
2024-06-09 01:14:48 +0000

    adding logo and header

--- net-cl/src/Header.js
+++ net-cl/src/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import './Header.css';
 function Header() {
   return (
-    <div>Header</div>
+    <div className='nav'>
+      <img className='nav_logo' src="https://ik.imagekit.io/Prince/Logonetflix.png?updatedAt=1717895518034" alt="Netflix logo" />
+    </div>
   )
 }
 
-export default Header
+export default Header;

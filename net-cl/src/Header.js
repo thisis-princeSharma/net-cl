0ab0635267a6d81c8cb93d47bbd88c4aa8717deb
PrinceSharma <121371635+thisis-princeSharma@users.noreply.github.com>
2024-06-09 01:34:44 +0000

    added black header

--- net-cl/src/Header.js
+++ net-cl/src/Header.js
@@ -1,11 +1,35 @@
-import React from 'react'
-import './Header.css';
+import React, { useEffect, useState } from "react";
+import "./Header.css";
+
+
 function Header() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(()=> {
+    window.addEventListener("scroll", () => {
+      if(window.scrollY > 100){
+        handleShow(true)
+      }
+      else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll")
+    }
+  }, []);
   return (
-    <div className='nav'>
-      <img className='nav_logo' src="https://ik.imagekit.io/Prince/Logonetflix.png?updatedAt=1717895518034" alt="Netflix logo" />
+    <div className={`nav ${show &&"nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://ik.imagekit.io/Prince/Logonetflix.png?updatedAt=1717895518034"
+        alt="Netflix logo"
+      />
+      <img
+        className="nav_avatar"
+        src="https://ik.imagekit.io/Prince/Netflix-avatar.png?updatedAt=1717895873305"
+        alt="Netflix smily avatar"
+      />
     </div>
-  )
+  );
 }
 
 export default Header;
